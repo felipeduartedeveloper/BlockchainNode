c06d102d67f100ad968b2d6aed009c5df55cfd62
Felipe Duarte <felipesilvapac@gmail.com>
2021-10-01 17:20:45 -0300

    chore: test blockchain with primary block

--- blockchain.js
+++ blockchain.js
@@ -4,4 +4,13 @@ class Blockchain{
   constructor(){
     this.chain = [Block.genesis()];
   }
+
+  addBlock(data){
+    const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
+    this.chain.push(block);
+
+    return block;
+  }
 }
+
+module.exports = blockchain;
---
+++ blockchain.test.js
@@ -0,0 +1,10 @@
+const Blockchain = require('./blockchain');
+
+describe('Blockchain', () => {
+  let bc;
+  beforeEach(() => {
+    bc = new Blockchain;
+  })
+
+  it('starts whith genesis block', () =>{})
+})
